fcd7877d3f516f26e6f56e59919eb19ec96667ed
Rooney <rooney.kamdem@student.vinci.be>
2024-11-09 16:14:43 +0100

    add : ligne des minutes par 5

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -7,6 +7,31 @@ describe("test", function() {
     });
     it("should return 0000", function() {
         const time = main.convertMinute("00:00:00");
+
         expect(time).toBe('0000');
     });
+
+    it("should return 00000000000", function() {
+        const time = main.convertMinuteBy5("00:00:00");
+
+        expect(time).toBe('00000000000');
+    })
+
+    it("should return Y0000000000", function() {
+        const time = main.convertMinuteBy5("00:05:00");
+
+        expect(time).toBe('Y0000000000');
+    })
+
+    it("should return YY000000000", function() {
+        const time = main.convertMinuteBy5("00:10:00");
+
+        expect(time).toBe('YY000000000');
+    })
+
+    it("should return YYR00000000", function() {
+        const time = main.convertMinuteBy5("00:15:00");
+
+        expect(time).toBe('YYR00000000');
+    })
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 
 export class Main {
-    // X : eteint, 0 : allumé
+    // 0 : eteint, X : allumé
     convertMinute (time) {
         const minutes = this.getMinutes(time)
         if (minutes === 1) return "Y000";
@@ -13,4 +13,22 @@ export class Main {
     getMinutes(time) {
         return parseInt(time.split(':')[1]);
     }
+
+    convertMinuteBy5 (time) {
+        const minutes = this.getMinutes(time)
+        if (minutes === 5) return "Y0000000000";
+        if (minutes === 10) return "YY000000000";
+        if (minutes === 15) return "YYR00000000";
+        if (minutes === 20) return "YYRY0000000";
+        if (minutes === 25) return "YYRYY000000";
+        if (minutes === 30) return "YYRYYR00000";
+        if (minutes === 35) return "YYRYYRY0000";
+        if (minutes === 40) return "YYRYYRYY000";
+        if (minutes === 45) return "YYRYYRYYR00";
+        if (minutes === 50) return "YYRYYRYYRY0";
+        if (minutes === 55) return "YYRYYRYYRYY";
+
+        return "00000000000";
+    }
+
 }
